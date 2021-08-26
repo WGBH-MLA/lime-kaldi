42e38c37ab5eb127ff0b0299b41775056c65fa42
foglabs <foglaboratories@gmail.com>
2021-08-26 12:45:23 -0400

    'update addjobs script and crons'

--- lime-kaldi-controller/app/addJobs.rb
+++ lime-kaldi-controller/app/addJobs.rb
@@ -8,21 +8,15 @@ require 'mysql2'
 require 'securerandom'
 @client = Mysql2::Client.new(host: "lime-kaldi-mysql", username: "root", database: "limekaldi", password: "", port: 3306)
 
-# main 4
+queue_number = 0
+
 File.readlines("Kansas_GUIDstoaddtoKaldi-split1.txt").each do |guid|
   uid = SecureRandom.uuid
-  query = %(INSERT INTO jobs (uid, status, input_filepath, input_bucketname, job_type) VALUES("#{uid}", #{JobStatus::New}, "#{guid.gsub("\n", "")}.mp3", "lime-kaldi-input", 0))
+  query = %(INSERT INTO jobs (uid, status, input_filepath, input_bucketname, job_type, queue_number) VALUES("#{uid}", #{JobStatus::New}, "#{guid.gsub("\n", "")}.mp3", "lime-kaldi-input", 0, #{queue_number}))
   puts query
   resp = @client.query(query)
   puts resp
-end
 
-
-# b4
-File.readlines("Kansas_GUIDstoaddtoKaldi-split2.txt").each do |guid|
-  uid = SecureRandom.uuid
-  query = %(INSERT INTO jobs (uid, status, input_filepath, input_bucketname, job_type) VALUES("#{uid}", #{JobStatus::New}, "#{guid.gsub("\n", "")}.mp3", "lime-kaldi-input", 1))
-  puts query
-  resp = @client.query(query)
-  puts resp
+  # toggle to evenly distrubte between queues
+  queue_number = queue_number == 0 ? 1 : 0
 end
